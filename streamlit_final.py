46d204770f451f1750eaeb7f5cde95b6a433cf1a
ElsyHK <79229070+ElsyHK@users.noreply.github.com>
2021-05-18 21:02:56 +0300

    Update streamlit_final.py

--- streamlit_final.py
+++ streamlit_final.py
@@ -282,62 +282,57 @@ if add_selectbox == 'Recommendation System':
     onehot_df = onehot_df.loc[:, onehot_df.columns.notnull()]
 
     # get frequent items
-    #freq_items=apriori(onehot_df, min_support=0.006, use_colnames=True,verbose=1)
-    
-#     def freq_itms(onehot_df):
-#         freq_items =apriori(onehot_df, min_support=0.006, use_colnames=True,verbose=1)
-#         return freq_items
-    
     freq_items = freq_itms(onehot_df)
-#     #get association rules
-#     rules=association_rules(freq_items, metric="confidence", min_threshold=0.25)
-#     # sorting by confidence
-#     rules=rules.sort_values(by=['confidence'], ascending=False)
-#     #rules_sorted_by_conf
+    
+    #get association rules
+    rules=association_rules(freq_items, metric="confidence", min_threshold=0.25)
+    # sorting by confidence
+    rules=rules.sort_values(by=['confidence'], ascending=False)
+    #rules_sorted_by_conf
 
-#     #transform items array to a list
-#     items = items.tolist()
-#     items.pop(4)
+    #transform items array to a list
+    items = items.tolist()
+    items.pop(4)
 
-#     # define length_antecedents, length_pconsequents
-#     rules['length_antecedents'] = rules['antecedents'].apply(lambda x: len(x))
-#     rules['length_consequents'] = rules['consequents'].apply(lambda x: len(x))
+    # define length_antecedents, length_pconsequents
+    rules['length_antecedents'] = rules['antecedents'].apply(lambda x: len(x))
+    rules['length_consequents'] = rules['consequents'].apply(lambda x: len(x))
 
 
 
-#     if st.checkbox("Increase Sales by Item"):
-#     # Recommendation System 1
-#         st.title("Increase Sales by Item")
-#         item = st.selectbox(
-#         'Select Item'.upper(),
-#         items
-#         )
+    if st.checkbox("Increase Sales by Item"):
+    # Recommendation System 1
+        st.title("Increase Sales by Item")
+        item = st.selectbox(
+        'Select Item'.upper(),
+        items
+        )
 
-#         # define consquent item to get the itemset
-#         rules_item=rules[rules['consequents']=={item}]
-#         X = rules_item.sort_values(by=['confidence'], ascending=False)
-#         st.header("Proposed Solution")
-#         st.write("Provide discounts on the below itemsets")
-#         X['antecedents']
+        # define consquent item to get the itemset
+        rules_item=rules[rules['consequents']=={item}]
+        X = rules_item.sort_values(by=['confidence'], ascending=False)
+        st.header("Proposed Solution")
+        st.write("Provide discounts on the below itemsets")
+        X['antecedents']
 
-#     if st.checkbox("Grow Share of Wallet"):
-#         # Recommendation System 2
-#         st.title("Grow Share of Wallet")
-#         st.subheader("Define Basket")
-#         # create a list with possible lenghth of item set
-#         nb_of_antecedents = st.selectbox("Select number of items".upper(), rules['length_antecedents'].unique())
+    if st.checkbox("Grow Share of Wallet"):
+        # Recommendation System 2
+        st.title("Grow Share of Wallet")
+        st.subheader("Define Basket")
+        # create a list with possible lenghth of item set
+        nb_of_antecedents = st.selectbox("Select number of items".upper(), rules['length_antecedents'].unique())
 
-#         # Display items
-#         antecedents = set()
-#         for i in range(nb_of_antecedents):
-#             itemi = st.selectbox(
-#             " Select item ".upper()+str(i+1),
-#             items
-#             )
-#             antecedents.add(itemi)
+        # Display items
+        antecedents = set()
+        for i in range(nb_of_antecedents):
+            itemi = st.selectbox(
+            " Select item ".upper()+str(i+1),
+            items
+            )
+            antecedents.add(itemi)
 
-#         st.write('Items in Basket:',antecedents)
-#         st.header("Proposed Solution")
-#         st.write("Provide discounts on the below items")
-#         predicted_item = rules[rules['antecedents']== antecedents]
-#         predicted_item['consequents']
+        st.write('Items in Basket:',antecedents)
+        st.header("Proposed Solution")
+        st.write("Provide discounts on the below items")
+        predicted_item = rules[rules['antecedents']== antecedents]
+        predicted_item['consequents']
